ed8c23104e57cf945c59ebf5f1cc666d6e2084e7
jesusfonser <jesusfonseruma@gmail.com>
2025-10-11 13:58:02 +0200

    feat: exercise 05 completed

---
+++ Javascript/Sheet01/ex05/main.js
@@ -0,0 +1,9 @@
+let edad = parseInt(prompt("¡Dame una edad!"));
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else if (edad < 18) {
+    console.log("Eres menor de edad");
+} else {
+    console.log("Edad no especificada");
+}
