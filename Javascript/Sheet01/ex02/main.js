ed815306f23f503c34095f7f2f24afed527036cc
jesusfonser <jesusfonseruma@gmail.com>
2025-10-11 13:30:53 +0200

    feat: exercise 02 completed

---
+++ Javascript/Sheet01/ex02/main.js
@@ -0,0 +1,15 @@
+let primero = "Hola profe";
+let segundo = 67;
+let tercero = true;
+let cuarto = undefined;
+let quinto = null;
+let sexto = Symbol();
+let septimo = 9007199254740991n;
+
+console.log(typeof primero);
+console.log(typeof segundo);
+console.log(typeof tercero);
+console.log(typeof cuarto);
+console.log(typeof quinto);
+console.log(typeof sexto);
+console.log(typeof septimo);
