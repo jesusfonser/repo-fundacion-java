2e4a613dd8ec34a0ed61da972ebb645db5682e3b
jesusfonser <jesusfonseruma@gmail.com>
2025-10-11 14:09:12 +0200

    feat: exercise 06 completed

---
+++ Javascript/Sheet01/ex06/main.js
@@ -0,0 +1,15 @@
+const dia = "lunes"
+
+switch (dia) {
+    case "lunes":
+        console.log("Inicio de semana");
+        break;
+
+    case "viernes":
+        console.log("Cierre de semana");
+        break;
+    
+    default:
+        console.log("Día intermedio");
+        break;
+}
