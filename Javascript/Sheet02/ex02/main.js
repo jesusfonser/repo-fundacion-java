98f7666b56c09f00bcc1ce757a5cfb3f9bc0ef3d
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-10-14 16:45:32 +0200

    feat: completed etiquetar and sumatorio functions

---
+++ Javascript/Sheet02/ex02/main.js
@@ -0,0 +1,17 @@
+function etiquetar(texto='N/A', prefijo=`TS-${Date.now()}`){
+    return `${prefijo}:${texto}`
+}
+
+console.log(/^TS-\d+:N\/A$/.test(etiquetar()));
+
+function sumatorio(base=0, ...nums){
+    const final = nums.reduce((a, b)=>{
+        if(typeof(b) != "number"){
+            return a;
+        } 
+            return a + b}, base)
+
+    return final
+}
+
+console.log(sumatorio(10, 1, 2, '3', 4)); // 17
