d90fb8ee137420b197fd8da5b4acf095383f529f
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-10-14 16:16:19 +0200

    feat: exercise completed

---
+++ Javascript/Sheet02/ex01/main.js
@@ -0,0 +1,9 @@
+function formatearA(nombre, apellido){
+    return `${apellido}, ${nombre}`
+}
+
+const formatearB = function(nombre, apellido){
+    return `${apellido}, ${nombre}`
+}
+
+const formatearC = (nombre, apellido) => `${apellido}, ${nombre}`
