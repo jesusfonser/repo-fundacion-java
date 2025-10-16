9a628656b474d35a89ec6a99f9be293ca24839d4
jesusfonser <jesusfonseruma@gmail.com>
2025-10-16 12:35:04 +0200

    feat: exercise 01 finally completed

--- Javascript/Sheet04/ex01/main.js
+++ Javascript/Sheet04/ex01/main.js
@@ -14,9 +14,16 @@ const li2 = document.createElement("li");
 li2.setAttribute("class", "tarea");
 li2.innerText = "Practicar DOM";
 
+const contador = document.createElement("span");
+
 
 ul.appendChild(li1);
 ul.appendChild(li2);
 
 contenedor.appendChild(h2);
+contenedor.appendChild(contador);
 contenedor.appendChild(ul);
+
+const tareas = document.querySelectorAll("li");
+tareas.length == 0 ? contador.innerText = "No hay tareas pendientes" :
+    contador.innerText = `Tareas pendientes: ${tareas.length}`;
