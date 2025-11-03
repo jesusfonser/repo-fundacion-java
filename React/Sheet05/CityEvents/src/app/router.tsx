cace43293cf6346ded7e948c9cee1899212a5b3e
jesusfonser <jesusfonseruma@gmail.com>
2025-11-03 12:03:21 +0100

    feat: complete exercise 1

--- React/Sheet05/CityEvents/src/app/router.tsx
+++ React/Sheet05/CityEvents/src/app/router.tsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./features/home/routes/Home.tsx";
-import Explore from "./features/explore/routes/Explore.tsx";
 import Help from "./features/help/routes/Help.tsx";
 import RootLayout from "./layouts/RootLayout.tsx";
 import ErrorPage from "./features/error/routes/Error.tsx"
+import ExploreLayout from "./layouts/ExploreLayout.tsx";
 
 
 export const router = createBrowserRouter([
     {
         path: "/", element: <RootLayout></RootLayout>, errorElement: <ErrorPage></ErrorPage>, children: [
-            { index: true, element: <Home></Home> },
-            { path: "explore", element: <Explore></Explore> },
+            { index: true, element: <Home></Home>} ,
+            {
+                path: "explore", element: <ExploreLayout></ExploreLayout>, children: [
+                    { path: "categories", element: <ExploreCategories></ExploreCategories> },
+                    { path: "cities", element: <ExploreCities></ExploreCities>}
+            ] },
             { path: "help", element: <Help></Help>}
     ]}
 ])
