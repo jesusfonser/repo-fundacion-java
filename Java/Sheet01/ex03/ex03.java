0653aae121a9b3441f58cf541b7950e677507d53
jesusfonser <jesusfonseruma@gmail.com>
2025-11-09 22:56:54 +0100

    feat: ex03 completed

---
+++ Java/Sheet01/ex03/ex03.java
@@ -0,0 +1,5 @@
+public class ex03 {
+    public static void main(String[] args){
+        System.out.println("Nombre del usuario: Jesús Fonseca Serrano\nSistema operativo: Ubuntu\nVersión de Java: Java 17");
+    }
+}
